20191fb14eed7dcfe1c7ed6c73a5a4a2c1e037ac
Fabian Sevilla <fabsevilla@yahoo.com>
2024-10-21 15:00:27 -0500

    First Commit for TicTacToe
    First commit for TicTacToeProject, first button added

--- practicereact/src/App.js
+++ practicereact/src/App.js
@@ -1,27 +1,5 @@
 import './App.css';
-import { useState } from 'react';
 
-export default function MyApp() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <div>
-      <h1>Counters that update together</h1>
-      <MyButton count={count} onClick={handleClick} />
-      <MyButton count={count} onClick={handleClick} />
-    </div>
-  );
+export default function Square() {
+  return <button className="square">X</button>;
 }
-
-function MyButton({ count, onClick }) {
-  return (
-    <button onClick={onClick}>
-      Clicked {count} times
-    </button>
-  );
-}
-
